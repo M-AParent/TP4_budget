75d1e9d3f565ef1d34d037a6fb0cc0da275f005b
M-AParent <marc-alexandre.parent@polymtl.ca>
2022-11-02 16:11:39 -0400

    Partie C sans input

--- myScriptWithInput.py
+++ myScriptWithInput.py
@@ -1,8 +1,11 @@
 import os
+import sys
 
 
 def main():
-    os.system('git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
+    badhash = sys.argv[1]
+    goodhash = sys.argv[2]
+    os.system('git bisect start %s %s'%(badhash, goodhash))
     os.system('git bisect run python manage.py test')
     os.system('git bisect reset')
 
